82be7648cc4712c2e69b8ec2893ec13cb688bded
Ritho <palvarez@ritho.net>
2020-03-12 15:57:15 +0100

    Try to get properly the trello url.

--- trello.pl
+++ trello.pl
@@ -40,14 +40,13 @@ sub pr_event {
 
 	my $card;
 	# print $event_data->{pull_request}->{body} . "\n";
-	if ($event_data->{pull_request}->{body} =~ m# (https://trello.com.*) #g) {
-		my $trello_url = $1;
-		print "$trello_url\n";
+	my ($trello_url) = ($event_data->{pull_request}->{body} =~ m# (https://trello.com.*) #g);
+	if (defined($trello_url)) {
+		print "Trello url: $trello_url\n";
 		$card = $trello->searchCardByShortUrl($trello_url);
 	}
 
 	unless (defined($card->{id})) {
-		print "$title\n";
 		$card = $trello->searchCardByName($title);
 	}
 
