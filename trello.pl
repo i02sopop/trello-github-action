a08deb43946fc8f26d30dbf32f8f5a5b02cf1404
Ritho <pablo.alvarez@lana.xyz>
2020-03-13 19:24:46 +0100

    Add some actions to implement during a PR event.

--- trello.pl
+++ trello.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use Switch;
 use JSON;
 use Data::Dumper;
 use Trello;
@@ -29,6 +30,10 @@ our $auth_header = "Authorization: token $ENV{'GITHUB_TOKEN'}";
 sub pr_event {
 	my $event_data = shift;
 
+	if ($event_data->{action} eq "edited") {
+		# XXX: Check if the PR has assign a previous card to undo the changes.
+	}
+
 	# Update the trello card with the PR url.
 	my $card;
 	my ($trello_url) = ($event_data->{pull_request}->{body} =~ m{ (https://trello.com.*) });
@@ -39,6 +44,14 @@ sub pr_event {
 	unless (defined($card->{id})) {
 		my $title = $event_data->{pull_request}->{title};
 		$card = $trello->searchCardByName($title);
+	}
+
+    if ($event_data->{action} eq "labeled") {
+		# If it has the bug label we create a card in Trello.
+	}
+
+    if ($event_data->{action} eq "unlabeled") {
+		# If we remove the bug label, we need to check if the card needs to be removed.
 	}
 
 	# If we don't have a card we don't have anything to do.
@@ -53,32 +66,55 @@ sub pr_event {
 
 	# Assign the trello card to the user that does the PR.
 	my $actor = $trello->searchMember($ENV{'GITHUB_ACTOR'});
+	unless (defined($actor->{id})) {
+		my $user_url = "${uri}/users/" . $ENV{'GITHUB_ACTOR'};
+		my $user_data = decode_json(`curl -sSL -H "$auth_header" -H "$api_header" "$user_url"`);
+		$actor = $trello->searchMember($user_data->{email});
+	}
+	
 	if (defined($actor->{id})) {
 		die "Error assigning the card to " . $actor->{username} . "\n"
 			unless ($trello->addCardMemberById($card->{id}, $actor->{id}));
 	}
 
-	my $user_url = "${uri}/users/" . $ENV{'GITHUB_ACTOR'};
-	my $user_data = decode_json(`curl -sSL -H "$auth_header" -H "$api_header" "$user_url"`);
-	$actor = $trello->searchMember($user_data->{email});
-	if (defined($actor->{id})) {
-		die "Error assigning the card to " . $actor->{username} . "\n"
-			unless ($trello->addCardMemberById($card->{id}, $actor->{id}));
+	if ($event_data->{action} eq "assigned") {
+		# We add the asignee to the card members.
 	}
 
-	# my $url = "${uri}/repos/$ENV{'GITHUB_REPOSITORY'}/pulls";
+	if ($event_data->{action} eq "unassigned") {
+		# We remove the asignee from the card members.
+	}
 
-	# my $comments_url = $event_data->{pull_request}->{comments_url};
-	# my $comments = decode_json(`curl -sSL -H "$auth_header" -H "$api_header" "$comments_url"`);
-	# print Dumper($event_data->{pull_request});
+	if ($event_data->{action} eq "review_requested") {
+		# We add the reviewer to the card members.
+	}
 
-	# my $ref = $ENV{'GITHUB_REF'}; # something like refs/pull/4/merge.
-	# $event_data->{sender}->{id}
-	# $event_data->{review_comments_url}
-	# $event_data->{labels}
-	# $event_data->{merged_by}
-	# $event_data->{review_comments}
-	# $event_data->{closed_at}
+	if ($event_data->{action} eq "review_request_removed") {
+		# We remove the reviewer from the card members.
+	}
+
+	# Move the card depending on the action.
+	switch ($event_data->{action}) {
+		case "created" {
+			# We move the card to work in progress.
+		}
+		case "edited" {
+			# The edition could add a new link to the card, so we move it to the
+			# right state.
+		}
+		case "opened" {
+			# We move the card to move in progress.
+		}
+		case "reopened" {
+			# We move the card to move in progress.
+		}
+		case "closed" {
+			# Check if it's merged or not to move the card to DONE or to TODO.
+		}
+		case "ready_for_review" {
+			# We move the card to the ready for review list if exists.
+		}
+	}
 
 	exit(-1);
 }
@@ -86,10 +122,52 @@ sub pr_event {
 # print "Out Environment: " . Dumper(%ENV) . "\n";
 my $event_name=$ENV{'GITHUB_EVENT_NAME'};
 my $event_data=decode_json(`jq --raw-output . "$ENV{'GITHUB_EVENT_PATH'}"`);
-print "Event data: " . Dumper($event_data) . "\n";
+#print "Event data: " . Dumper($event_data) . "\n";
 
 if ($event_name eq 'pull_request') {
 	pr_event($event_data);
 } else {
 	print "Event $event_name without action.\n";
 }
+
+# event payload:  Issues
+# Activity types:
+# - opened
+# - edited
+# - deleted
+# - transferred
+# - pinned
+# - unpinned
+# - closed
+# - reopened
+# - assigned
+# - unassigned
+# - labeled
+# - unlabeled
+# - locked
+# - unlocked
+# - milestoned
+# - demilestoned
+# GITHUB_SHA: Last commit on default branch
+# GITHUB_REF: Default branch
+
+# Webhook event payload: Project
+# Activity types:
+# - created
+# - updated
+# - closed
+# - reopened
+# - edited
+# - deleted
+# GITHUB_SHA: Last commit on default branch
+# GITHUB_REF: Default branch
+
+# Webhook event payload: project_card
+# Activity types:
+# - created
+# - moved
+# - converted to an issue
+# - edited
+# - deleted
+# GITHUB_SHA: Last commit on default branch
+# GITHUB_REF: Default branch
